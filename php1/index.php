a3c615be47cf0654406d755077b87d6886a36d46
Jennifer <jksulkow@gmail.com>
2012-09-26 18:25:48 -0400

    Class work

---
+++ php1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?
+    $square = 4 * 4;
+    
+    # String
+    $name = "Susan";
+    
+    # Int
+    $age = 42;
+    
+    # Float
+    $weight = 250.43;
+    
+    # Boolean
+    $logged_in = FALSE;
+    
+    $quarter = .25;
+    $dime   = .10;
+    $nickel = .05;
+    
+    $total = (4 * $quarter) + (5 * $dime);
+    
+    
+    ?>
+</head>
+<body>
+    Total change: <?=$total?>
+    
+    The square of 4 is <?=$square?>
+</body>
+
+</html>
