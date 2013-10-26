2eeceb223176773264118908d1fdb3be02161d58
Jennifer <jksulkow@gmail.com>
2013-10-26 16:40:41 -0400

    Card generator

---
+++ pickup-php/v_cardgenerator.php
@@ -0,0 +1,38 @@
+<div id='wrapper'>
+
+	<!-- HEADING -->
+	<h1>Card Generator</h1>
+
+	<!-- CARD SETTINGS ON THE LEFT -->
+	<div id='left-side'>
+		<h2>Pick a color</h2>
+		<div class='color-choice' id='red'></div>
+		<div class='color-choice' id='orange'></div>
+		<div class='color-choice' id='yellow'></div>
+		<div class='color-choice' id='green'></div>
+		<div class='color-choice' id='blue'></div>
+		<div class='color-choice' id='indigo'></div>
+		<div class='color-choice' id='violet'></div>
+		<br>
+
+		<h2>Pick a texture</h2>
+
+		<h2>Pick a message</h2>
+
+		<h2>Enter your recipient</h2>
+
+		<h2>Add some graphics</h2>
+	</div>
+
+	<!-- CARD SETTINGS ON THE RIGHT -->
+	<div id='right-side'>
+		<div id='card'>
+        	<div id='canvas'></div>
+        </div>
+	</div>
+
+	<!-- FOOTER -->
+	<div id='footer'></div>
+
+
+</div>
